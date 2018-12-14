3b40c29a3ddfdc67875579187ab8505c3e2cfce1
Alex Bosworth <alex.bosworth@gmail.com>
2018-12-13 16:38:40 -0800

    write linking rows when setting channel record

--- records/set_channel_record.js
+++ records/set_channel_record.js
@@ -2,6 +2,8 @@ const {chanIdHexLen} = require('./constants');
 const chainIds = require('./conf/networks').chain_ids;
 const {chansDb} = require('./constants');
 const {lmdb} = require('./../lmdb');
+const {nodeChannelsDb} = require('./constants');
+const {nodeNumbers} = require('./constants');
 const {putLmdbItem} = require('./../lmdb');
 
 /** Directly set a channel record
@@ -39,33 +41,47 @@ module.exports = args => {
     throw new Error('ExpectedNetworkToSetChannel');
   }
 
+  const key = `${chainIds[args.network]}${args.id}`;
   const [policy1, policy2] = args.policies;
 
   try {
+    const channel = {
+      capacity: args.capacity,
+      node1_base_fee_mtokens: policy1.base_fee_mtokens,
+      node1_cltv_delta: policy1.cltv_delta,
+      node1_fee_rate: policy1.fee_rate,
+      node1_is_disabled: policy1.is_disabled,
+      node1_min_htlc_tokens: policy1.min_htlc_mtokens,
+      node1_public_key: policy1.public_key,
+      node1_updated_at: args.updated_at,
+      node2_base_fee_mtokens: policy2.base_fee_mtokens,
+      node2_cltv_delta: policy2.cltv_delta,
+      node2_fee_rate: policy2.fee_rate,
+      node2_is_disabled: policy2.is_disabled,
+      node2_min_htlc_tokens: policy2.min_htlc_mtokens,
+      node2_public_key: policy2.public_key,
+      node2_updated_at: args.updated_at,
+      transaction_id: args.transaction_id,
+      transaction_vout: args.transaction_vout,
+    };
+
     putLmdbItem({
+      key,
       db: chansDb,
-      key: `${chainIds[args.network]}${args.id}`,
       lmdb: lmdb({path: args.lmdb_path}),
-      value: {
-        capacity: args.capacity,
-        node1_base_fee_mtokens: policy1.base_fee_mtokens,
-        node1_cltv_delta: policy1.cltv_delta,
-        node1_fee_rate: policy1.fee_rate,
-        node1_is_disabled: policy1.is_disabled,
-        node1_min_htlc_tokens: policy1.min_htlc_mtokens,
-        node1_public_key: policy1.public_key,
-        node1_updated_at: args.updated_at,
-        node2_base_fee_mtokens: policy2.base_fee_mtokens,
-        node2_cltv_delta: policy2.cltv_delta,
-        node2_fee_rate: policy2.fee_rate,
-        node2_is_disabled: policy2.is_disabled,
-        node2_min_htlc_tokens: policy2.min_htlc_mtokens,
-        node2_public_key: policy2.public_key,
-        node2_updated_at: args.updated_at,
-        transaction_id: args.transaction_id,
-        transaction_vout: args.transaction_vout,
-      },
+      value: channel,
     });
+
+    // Create links from the pubkeys to the channel id
+    nodeNumbers
+      .map(n => channel[`node${n}_public_key`])
+      .filter(n => !!n)
+      .map(pubKey => ({
+        db: nodeChannelsDb,
+        key: `${pubKey}${key}`,
+        lmdb: lmdb({path: args.lmdb_path}),
+      }))
+      .forEach(({db, key, lmdb}) => putLmdbItem({db, key, lmdb, value: {}}));
   } catch (err) {
     throw new Error('UnexpectedFailureSettingChannelRecord');
   }
